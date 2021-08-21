899bf76caa383a13af075d538f389671c040464b
HcjunioR <hcjuniorxd@gmail.com>
2021-08-20 23:47:06 -0300

    Atividades 02 e 03 feitas, 3 novas servlets criadas, arquivo README.md modificado

---
+++ src/main/java/br/edu/ceub/atividade02/servletQualMeuNome.java
@@ -0,0 +1,40 @@
+package br.edu.ceub.atividade02;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class servletQualMeuNome
+ */
+@WebServlet("/servletQualMeuNome")
+public class servletQualMeuNome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public servletQualMeuNome() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().write("<html><body><p><a href='servletHelitonChagas'>Qual meu nome?</a></p></body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
